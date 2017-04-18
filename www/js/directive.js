28ab3a9f45c90fd7223bb734682275c49ceb7c90
wang peipei <wangpp1995@gmail.com>
2017-04-18 19:31:36 +0800

    增加表单验证

--- www/js/directive.js
+++ www/js/directive.js
@@ -1,21 +1,39 @@
-angular.module('directive',[])
-.directive('dateFormat', ['$filter',function($filter) {  
-    var dateFilter = $filter('date');  
-    return {  
-        require: 'ngModel',  
-        link: function(scope, elm, attrs, ctrl) {  
-  
-            function formatter(value) {  
-                return dateFilter(value, 'yyyy-MM-dd'); //format  
-            }  
-  
-            function parser() {  
-                return ctrl.$modelValue;  
-            }  
-  
-            ctrl.$formatters.push(formatter);  
-            ctrl.$parsers.unshift(parser);  
-  
-        }  
-    };  
-}]); 
+angular.module('directive', [])
+    .directive('dateFormat', ['$filter', function($filter) {
+        var dateFilter = $filter('date');
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+
+                function formatter(value) {
+                    return dateFilter(value, 'yyyy-MM-dd'); //format  
+                }
+
+                function parser() {
+                    return ctrl.$modelValue;
+                }
+
+                ctrl.$formatters.push(formatter);
+                ctrl.$parsers.unshift(parser);
+
+            }
+        };
+    }])
+    .directive('pwCheck', [function() {
+        return {
+            restrict: 'A',
+            require: "ngModel",
+            link: function(scope, elem, attrs, ctrl) {
+                var otherInput = elem.inheritedData("$formController")[attrs.pwCheck];
+                ctrl.$parsers.push(function(value) {
+                    ctrl.$setValidity("pwCheck", value === otherInput.$viewValue);
+                    return value;
+                });
+
+                otherInput.$parsers.push(function(value) {
+                    ctrl.$setValidity("pwCheck", value === ctrl.$viewValue);
+                    return value;
+                });
+            }
+        };
+    }]);;
